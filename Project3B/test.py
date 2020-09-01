206810dd9055877551a071209eb9d5acf209c521
TonyYounCH <tonyyoun2@gmail.com>
2020-09-01 09:08:44 +0900

    Test

--- Project3B/test.py
+++ Project3B/test.py
@@ -35,13 +35,7 @@ for i in lines:
     if summary_type == 'SUPERBLOCK': # get basic information
         total_blocks = int(field[1])
         total_inodes = int(field[2])
-        block_size = int(fields[3])
-        inode_size = int(fields[4])
 
-    elif summary_type == 'GROUP':
-        num_blocks = int(fields[2])
-        num_inodes = int(fields[3])
-        first_valid_block = int(fields[8]) + inode_size * num_inodes / block_size
 
     elif summary_type == 'BFREE': # put in free blocks list
         bfree.add(int(field[1])) 
